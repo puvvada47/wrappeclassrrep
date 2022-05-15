0c822799391f3ade8d99b153c4944761ed8a9a82
puvvada47 <puvvada1210609147@gmail.com>
2022-05-15 11:19:00 +0530

    wrapper class code

---
+++ wrapperclass/src/wrapperclass/Prmitivetowrapper.java
@@ -0,0 +1,16 @@
+package wrapperclass;
+
+public class Prmitivetowrapper {
+	public static void main(String args[]) {
+		// Converting int into Integer
+		int a = 20;
+		
+		Integer i = Integer.valueOf(a);// converting int into Integer
+		Integer j = a;// autoboxing, now compiler will write Integer.valueOf(a)
+						// internally
+
+		System.out.println(a + " " + i + " " + j);
+	}
+}  
+
+
